c164b6dde3e6e988bca629e1a417ef7d4bfa8eb0
jay <kjh338076@naver.com>
2019-02-10 17:19:29 +0900

    테스트용 외부설정

---
+++ src/test/java/me/jko/ApplicationTest.java
@@ -0,0 +1,27 @@
+package me.jko;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Autowired
+    Environment environment;
+
+    @Test
+    public void contextLoads() {
+
+        assertThat(environment.getProperty("jko.name"))
+                .isEqualTo("testJunhee");
+    }
+
+}

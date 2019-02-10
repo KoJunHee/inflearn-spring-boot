1560b290ab461fb18539165f3adf0e61902b9171
jay <kjh338076@naver.com>
2019-02-10 15:37:42 +0900

    mvn clean: remove target directory / mvn package: make target directory

--- src/main/java/me/jko/Application.java
+++ src/main/java/me/jko/Application.java
@@ -3,14 +3,20 @@ package me.jko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// 1. Component Scan
-// 2. Auto Configuration
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-//        springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
     }
 }

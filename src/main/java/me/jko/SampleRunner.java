f48d21752690f3d505adb930643e4e48521311e4
고준희(Junhee Ko)/광고플랫폼개발팀/11ST <jko@sk.com>
2019-02-11 08:28:05 +0900

    info log 찍기

--- src/main/java/me/jko/SampleRunner.java
+++ src/main/java/me/jko/SampleRunner.java
@@ -1,13 +1,18 @@
 package me.jko;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class SampleRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(SampleRunner.class);
 
     @Autowired
     String hello;
@@ -17,6 +22,14 @@ public class SampleRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+        logger.info("===============================");
+        logger.info(hello);
+        logger.info(jkoProperties.getFullName());
+        logger.info(jkoProperties.getName());
+        logger.info("===============================");
+
+
         System.out.println("=========================");
         System.out.println(hello);
         System.out.println(jkoProperties.getName());

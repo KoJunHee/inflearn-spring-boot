0e61e5da9c10592168ebe286650f186c3f97b485
jay <kjh338076@naver.com>
2019-02-10 16:03:12 +0900

    ApplicationStartingEvent

--- src/main/java/me/jko/Application.java
+++ src/main/java/me/jko/Application.java
@@ -2,14 +2,13 @@ package me.jko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
-                .sources(Application.class)
-                .run(args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new SampleListener());
+        springApplication.run(args);
     }
 }

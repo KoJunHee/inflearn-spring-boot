169e1cabdd6d5ec09c17ee6f5f462f3c9ebd1aef
jay <kjh338076@naver.com>
2019-02-10 16:04:53 +0900

    ApplicationStartedEvent

--- src/main/java/me/jko/Application.java
+++ src/main/java/me/jko/Application.java
@@ -8,7 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.addListeners(new SampleListener());
         springApplication.run(args);
     }
 }
--- src/main/java/me/jko/SampleListener.java
+++ src/main/java/me/jko/SampleListener.java
@@ -1,18 +1,18 @@
 package me.jko;
 
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-// ApplicationStartingEvent가 발생하면 이 리스너를 실행함
-// 그런데 ApplicationStartingEvent는 application context가 생성되기 전에 생성됨.. 그래서 실행이 안됨..
-// 그래서 springApplication.addListeners(new SampleListener()); 를 해야함
+// ApplicationStartedEvent 발생하면 이 리스너를 실행함
 @Component
-public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+public class SampleListener implements ApplicationListener<ApplicationStartedEvent> {
+
     @Override
-    public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
-        System.out.println("-------------------------");
-        System.out.println("Starting");
-        System.out.println("-------------------------");
+    public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
+        System.out.println("======================");
+        System.out.println("applicationStartedEvent");
+        System.out.println("======================");
     }
 }

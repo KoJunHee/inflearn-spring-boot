7c8d390c8136b11ce6367219c53df090efb44b58
jay <kjh338076@naver.com>
2019-02-10 16:06:26 +0900

    WebApplicationType

--- src/main/java/me/jko/Application.java
+++ src/main/java/me/jko/Application.java
@@ -1,6 +1,7 @@
 package me.jko;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,6 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
     }
 }

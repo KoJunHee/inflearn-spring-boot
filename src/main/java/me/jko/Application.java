8bc480e98a2f3d51d2e9c136335a0ef9f0a0a2c7
고준희(Junhee Ko)/광고플랫폼개발팀/11ST <jko@sk.com>
2019-02-12 08:56:18 +0900

    H2 database test

--- src/main/java/me/jko/Application.java
+++ src/main/java/me/jko/Application.java
@@ -1,9 +1,7 @@
 package me.jko;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class Application {

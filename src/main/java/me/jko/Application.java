216c13038ab92e69075b78c849d07d5f41c3816b
고준희(Junhee Ko)/광고플랫폼개발팀/11ST <jko@sk.com>
2019-02-12 08:36:22 +0900

    addResourceHandlers

--- src/main/java/me/jko/Application.java
+++ src/main/java/me/jko/Application.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JkoProperties.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
     }
 }

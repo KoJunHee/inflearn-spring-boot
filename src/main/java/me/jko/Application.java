99da430e54adf58b5e47f952149ff76eb50719c9
jay <kjh338076@naver.com>
2019-02-17 17:05:24 +0900

    jpa

--- src/main/java/me/jko/Application.java
+++ src/main/java/me/jko/Application.java
@@ -6,12 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JkoProperties.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE);
+//        springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
     }
 }

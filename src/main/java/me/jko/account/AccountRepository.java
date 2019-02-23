1149f6e0df67a1b2fbdb7cc6b43153b66517e661
jay <kjh338076@naver.com>
2019-02-23 15:57:59 +0900

    spring security

--- src/main/java/me/jko/account/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package me.jko.account;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository  extends JpaRepository<Account, Long> {
-    Account findByUsername(String username);
-
-}
